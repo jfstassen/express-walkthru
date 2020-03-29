9d46ef817cacc15c49aa0538f2650f192f2e9523
jfstassen <jf.stassen@gmail.com>
2020-03-29 19:45:42 +0200

    add serve file statically

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const path = require("path")
+const path = require("path");
 
 const app = express();
 
@@ -8,14 +8,14 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public"))); // give access to public folder like css etc
 
-app.use('/admin', adminRoutes); //only routes starting with /admin will go in admin routes file
+app.use("/admin", adminRoutes); //only routes starting with /admin will go in admin routes file
 app.use(shopRoutes);
 
-app.use((req, res, next)=> {
+app.use((req, res, next) => {
   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
-  
-})
+});
 // app.use('/add-product', (req, res, next) => {
 //   res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">add product</button></form>')
 // })
